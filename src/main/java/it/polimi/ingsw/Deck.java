0ee8dd16bd7499a0984f0e2e3b8bf4ae3a2c2de4
Alberto Fanton <alberto.fanton@protonmail.com>
2021-03-21 22:08:59 +0100

    Add Deck

---
+++ src/main/java/it/polimi/ingsw/Deck.java
@@ -0,0 +1,90 @@
+package it.polimi.ingsw;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * It is a container of a group of cards with some specific operations
+ * @param <T> the type of the card
+ */
+public class Deck<T>{
+    private Deque<T> cards;
+
+    /**
+     * Initializes an empty Deck
+     */
+    public Deck() {
+        // TODO not thread safe, check it later
+        cards = new ArrayDeque<>();
+    }
+
+    /** Initializes a deck from a collection
+     *
+     * @param cards cards used to initialize the deck
+     */
+    public Deck(Collection<? extends T> cards){
+        // TODO not thread safe, check it later
+        this.cards = new ArrayDeque<>(cards);
+    }
+
+    /**
+     * Shuffles the deck
+     */
+    public void shuffle() {
+        List<T> cards = (List<T>)Arrays.asList(this.cards.toArray());
+        Collections.shuffle(cards);
+        this.cards = new ArrayDeque<>(cards);
+    }
+
+    /**
+     * Checks if the deck is empty
+     * @return true if the deck is empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+    /**
+     * Draws a card from a deck, the card IS removed from the deck
+     * @return the picked card
+     * @throws NoSuchElementException is  the deck is empty
+     */
+    public T drawFirstCard() throws NoSuchElementException {
+        return cards.removeFirst();
+    }
+
+    /**
+     * Shows a card from the deck, the card IS NOT removed
+     * @return the first card of the deck
+     * @throws NoSuchElementException is  the deck is empty
+     */
+    public T showFirstCard() {
+        return cards.getFirst();
+    }
+
+    /**
+     * Adds a card to the deck as the first
+     * @param card the card to insert in the deck, as first
+     */
+    public void addCard(T card) {
+        cards.addFirst(card);
+    }
+
+    public void addAll(Collection<? extends T> cards) {
+        cards.forEach(x -> this.addCard(x));
+    }
+
+    /**
+     * Returns the length of the deck
+     * @return the length of the deck
+     */
+    public int size() {
+        return cards.size();
+    }
+
+    @Override
+    public String toString() {
+        String s = "Deck: " + cards.stream().map(Object::toString).collect(Collectors.joining(", "));
+        return s;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/DeckTest.java
@@ -0,0 +1,73 @@
+package it.polimi.ingsw;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class DeckTest {
+    private Deck<Object> deck;
+
+    @Before
+    public void setUp() {
+        this.deck  = new Deck<>();
+    }
+
+    @Test
+    public void testShuffle() {
+        assertEquals(0, this.deck.size());
+        this.deck.shuffle();
+        assertEquals(0, this.deck.size());
+        this.deck.addAll(Arrays.asList(1,2,3,4,5));
+        assertEquals(5, this.deck.size());
+        this.deck.shuffle();
+        assertEquals(5, this.deck.size());
+    }
+
+    @Test
+    public void testCreateFromCollection() {
+        var deck = new Deck(Arrays.asList(1,2,3,4,5));
+        assertEquals(5, deck.size());
+    }
+
+    @Test
+    public void testIsEmptyAddCard() {
+        assertEquals(true, deck.isEmpty());
+        deck.addCard(42);
+        assertEquals(false, deck.isEmpty());
+
+    }
+
+    @Test
+    public void testDrawFirstCard() {
+        Object o = new Object();
+        this.deck.addCard(42);
+        this.deck.addCard(o);
+        assertEquals(2, this.deck.size());
+        Object o1 = this.deck.drawFirstCard();
+        assertEquals(o1, o);
+        assertEquals(1, this.deck.size());
+    }
+
+    @Test
+    public void testShowFirstCard() {
+        Object o = new Object();
+        this.deck.addCard(42);
+        this.deck.addCard(o);
+        assertEquals(2, this.deck.size());
+        Object o1 = this.deck.showFirstCard();
+        assertEquals(o1, o);
+        assertEquals(2, this.deck.size());
+    }
+
+
+    @Test
+    public void testToString() {
+        this.deck.addCard(42);
+        this.deck.addCard(43);
+        assertEquals("Deck: 43, 42", this.deck.toString());
+    }
+
+}
