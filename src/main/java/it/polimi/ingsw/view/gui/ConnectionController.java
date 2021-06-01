7676f48e237e2e61ceaa8f5016da54928294841b
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2021-06-01 17:45:43 +0200

    Fix Gui

---
+++ src/main/java/it/polimi/ingsw/view/gui/ConnectionController.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class ConnectionController{
+    @FXML
+    private TextField ipField;
+    @FXML
+    private Button connectionButton;
+
+
+    @FXML
+    public void initialize(){
+        bindEvents();
+    }
+
+    private void bindEvents(){
+        connectionButton.addEventHandler(MouseEvent.MOUSE_CLICKED,event -> setIp());
+    }
+
+    private void setIp(){
+        String ip = ipField.getText();
+        GUIController.getInstance().setIp(ip);
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/view/gui/LoginController.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.view.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController{
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField gameIDField;
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void initialize(){
+        bindEvents();
+    }
+
+    private void bindEvents(){
+        loginButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> setCredentials());
+    }
+
+    private void setCredentials(){
+        String username = usernameField.getText();
+        GUIController.getInstance().setUsername(username);
+        String gameID = gameIDField.getText();
+        GUIController.getInstance().setGameID(gameID);
+        notifyAll();
+    }
+
+}
