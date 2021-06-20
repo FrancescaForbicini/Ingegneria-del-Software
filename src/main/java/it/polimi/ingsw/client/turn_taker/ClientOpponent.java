46e31dc7ae1bb7eb9d235ef2afbf74bb7546d06f
Alberto Fanton <alberto.fanton@protonmail.com>
2021-06-20 12:10:45 +0200

    Add ClientTurnTaker

---
+++ src/main/java/it/polimi/ingsw/client/turn_taker/ClientOpponent.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.client.turn_taker;
+
+public class ClientOpponent extends ClientTurnTaker{
+
+}
---
+++ src/main/java/it/polimi/ingsw/client/turn_taker/ClientTurnTaker.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.client.turn_taker;
+
+public abstract class ClientTurnTaker {
+}
