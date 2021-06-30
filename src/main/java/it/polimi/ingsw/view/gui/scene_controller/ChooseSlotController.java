d2a78a6bc50aa3cd27c0e1b97951ce680b03a30e
Francesca <francesca.forbicini@mail.polimi.it>
2021-06-30 22:39:55 +0200

    Fix ChooseSlot

---
+++ src/main/java/it/polimi/ingsw/view/gui/scene_controller/ChooseSlotController.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw.view.gui.scene_controller;
+
+import it.polimi.ingsw.model.board.DevelopmentSlot;
+import it.polimi.ingsw.view.gui.GUIController;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+
+public class ChooseSlotController {
+
+    private final ArrayList<DevelopmentSlot> developmentSlots;
+
+    @FXML
+    private GridPane slots;
+
+    @FXML
+    private Button slot1;
+
+    @FXML
+    private Button slot2;
+
+    @FXML
+    private Button slot3;
+
+    public ChooseSlotController(ArrayList<DevelopmentSlot> developmentSlots) {
+        this.developmentSlots = developmentSlots;
+    }
+    public void initialize() {
+        int numberSlot = 0;
+        for (Node node: slots.getChildren()){
+            Button choose = (Button) node;
+            if (developmentSlots.get(numberSlot).getSlotID() == numberSlot){
+                if (developmentSlots.get(numberSlot).showCardOnTop().isPresent()) {
+                    ImageView imageView = new ImageView(developmentSlots.get(numberSlot).getCards().getFirst().getPath());
+                    imageView.setFitWidth(slots.getWidth());
+                    imageView.setFitHeight(slots.getHeight());
+                    choose.setGraphic(imageView);
+                }
+                else{
+                    choose.setText("Choose");
+                }
+                int finalNumberSlot = numberSlot;
+                choose.setOnAction(actionEvent -> setSlot(finalNumberSlot));
+            }
+            numberSlot++;
+        }
+    }
+    private void setSlot(int slotIndex){
+        GUIController.getInstance().setPickedIndex(slotIndex);
+    }
+}
