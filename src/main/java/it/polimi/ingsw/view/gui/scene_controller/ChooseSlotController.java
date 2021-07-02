516d970b4a063f15737b6fc913a29e5cd95399e2
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2021-07-02 16:44:27 +0200

    Fix ChooseSlot scene

--- src/main/java/it/polimi/ingsw/view/gui/scene_controller/ChooseSlotController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scene_controller/ChooseSlotController.java
@@ -1,11 +1,16 @@
 package it.polimi.ingsw.view.gui.scene_controller;
 
 import it.polimi.ingsw.model.board.DevelopmentSlot;
+import it.polimi.ingsw.model.cards.DevelopmentCard;
 import it.polimi.ingsw.view.gui.GUIController;
 import it.polimi.ingsw.view.gui.SceneManager;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
@@ -13,63 +18,45 @@ import java.util.ArrayList;
 public class ChooseSlotController {
 
     private final ArrayList<DevelopmentSlot> developmentSlots;
+    private final double HEIGHT = 150;
+    private final double WIDTH = 100;
+
 
     @FXML
     private GridPane slots;
-
-    @FXML
-    private Button slot0;
-
-    @FXML
-    private Button slot1;
-
-    @FXML
-    private Button slot2;
 
     public ChooseSlotController(ArrayList<DevelopmentSlot> developmentSlots) {
         this.developmentSlots = developmentSlots;
     }
     public void initialize() {
         slots.setGridLinesVisible(true);
-        for (int i = 0; i < 3 ; i++){
-            int finalI = i;
-            Label choose = new Label();
-            DevelopmentSlot developmentSlot = null;
-            if (developmentSlots.stream().anyMatch(slot -> slot.getSlotID() == finalI)){
-                for (int numberSlot = 0; numberSlot < 3; numberSlot++ ) {
-                    if (developmentSlots.get(numberSlot).getSlotID() == i) {
-                        developmentSlot = developmentSlots.get(numberSlot);
-                        break;
-                    }
-                }
-                if (developmentSlot== null)
-                    developmentSlot = developmentSlots.get(0);
-                if(developmentSlot.showCardOnTop().isPresent())
-                    choose.setGraphic(SceneManager.getInstance().getNode(developmentSlots.get(i).getCards().getLast().getPath(),150,100));
-                else
-                    choose.setText("Choose");
-                switch (developmentSlot.getSlotID()){
-                    case 0:
-                        slot0.setGraphic(choose);
-                        slot0.setOnAction(actionEvent -> setSlot(0));
-                        slot0.setDisable(false);
-                        break;
-                    case 1:
-                        slot1.setGraphic(choose);
-                        slot1.setOnAction(actionEvent -> setSlot(1));
-                        slot1.setDisable(false);
-                        break;
-                    case 2:
-                        slot2.setGraphic(choose);
-                        slot2.setOnAction(actionEvent -> setSlot(2));
-                        slot2.setDisable(false);
-                        break;
-                    default:
-                }
+        for(int i=0; i<developmentSlots.size(); i++){
+            FlowPane singleSlot = new FlowPane();
+            if(developmentSlots.get(i).showCardOnTop().isPresent()){
+                singleSlot.getChildren().add(getNodeSlot(developmentSlots.get(i).getCards().getLast(),i));
+            } else {
+                Button slotButton = new Button("Choose");
+                int finalI = i;
+                slotButton.setOnAction(actionEvent -> setSlot(finalI));
+                singleSlot.getChildren().add(slotButton);
             }
+            slots.add(singleSlot,i,0);
         }
     }
     private void setSlot(int slotIndex){
         GUIController.getInstance().setPickedIndex(slotIndex);
+    }
+
+    private Node getNodeSlot(DevelopmentCard developmentCard, int index){
+        FlowPane cardPane = new FlowPane();
+        Button cardButton = new Button();
+        if(developmentCard.getPath()!=null){
+            cardButton.setGraphic(SceneManager.getInstance().getNode(developmentCard.getPath(),HEIGHT, WIDTH));
+        } else {
+            cardButton.setText("Choose");
+        }
+        cardButton.setOnAction(actionEvent -> setSlot(index));
+        cardPane.getChildren().add(cardButton);
+        return cardPane;
     }
 }
